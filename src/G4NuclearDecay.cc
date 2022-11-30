5584b679e9ef0c936ee9196ec262a1f518dbaf91
Jeppe Schultz Nielsen <jeppe-schultz@hotmail.com>
2022-11-30 23:31:06 +0100

    Initial commit

---
+++ src/G4NuclearDecay.cc
@@ -0,0 +1,55 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  File:   G4NuclearDecay.cc                                                 //
+//  Author: D.H. Wright (SLAC)                                                //
+//  Date:   11 December 2014                                                  //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#include "G4NuclearDecay.hh"
+#include "G4SystemOfUnits.hh"
+
+G4NuclearDecay::G4NuclearDecay(const G4String& channelName,
+                               const G4RadioactiveDecayMode& aMode,
+                               const G4double& excitationE,
+                               const G4Ions::G4FloatLevelBase& flb)
+ : G4VDecayChannel(channelName), theMode(aMode), daughterEx(excitationE),
+   floatingLevel(flb), halflifeThreshold(nanosecond) 
+{}
+
+G4NuclearDecay::~G4NuclearDecay()
+{}
+
+G4double G4NuclearDecay::GetNeutronPenetrability(G4double E, G4double A1, G4double A2, G4int l){
+    G4double x = 1.4 * std::sqrt(2.*(A1*A2)/(A1+A2)*931.502*E)*(pow(A1,1./3.) + pow(A2,1./3.))/197329.;
+    if(l == 0){return 1;}
+    if(l == 1){return x*x/(1.+x*x);}
+    if(l == 2){return std::pow(x,4)/(9.+3.*std::pow(x,3)+std::pow(x,4)); }
+    if(l > 3){return std::pow(x,6)/(225.+45.*std::pow(x,2)+6*std::pow(x,4) + std::pow(x,6)); }
+    else return -1;
+}
